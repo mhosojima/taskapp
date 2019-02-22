7cad43a5d0ddd52a7cecbdac7d80a04444eb2dae
masahiko.hosojima <zero_001@qc5.so-net.ne.jp>
2019-02-22 09:44:46 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 細島誠彦 on 2019/02/22.
+//  Copyright © 2019 TransamManegemetnSystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
